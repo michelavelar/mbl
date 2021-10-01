bbb4932bd4c3b4891ada53df23a09a9b75fc8fa4
michelavelar <michel.avelar@gmail.com>
2021-10-01 12:31:06 -0300

    a

---
+++ v3/assets/res/js/custom.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  // hide #back-top first
+  $("#scroll-top").hide();
+  // fade in #back-top
+  $(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $('#scroll-top').fadeIn();
+      } else {
+        $('#scroll-top').fadeOut();
+      }
+    });
+    // scroll body to 0px on click
+    $('#scroll-top button').click(function () {
+      $('body,html').animate({
+        scrollTop: 0
+      }, 800);
+    });
+  });
+
+});
